44f895818df2df77d95c07442f91d1b3e6949595
beittur <bjornj12@ru.is>
2013-10-28 16:09:56 +0000

    succesfull mvn compile

--- src/main/java/is/ru/helloworld/HelloWorld.java
+++ src/main/java/is/ru/helloworld/HelloWorld.java
@@ -2,10 +2,10 @@ package is.ru.helloworld;
 
 public class HelloWorld
 {
-	Public static void main(String[] args)
+	public static void main(String[] args)
 	{
 		System.out.println("Hello world!");
 		World world = new World();
-		System.out.println(wordl.greet);
+		System.out.println(world.greet());
 	}
 }
--- src/main/java/is/ru/helloworld/World.java
+++ src/main/java/is/ru/helloworld/World.java
@@ -2,7 +2,7 @@ package is.ru.helloworld;
 
 public class World
 {
-	public string greet() 
+	public String greet() 
 	{
 		return "Hello world!";
 	}
